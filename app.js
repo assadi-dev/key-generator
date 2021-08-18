6ba21b58624b6b97b967948e3843efcb1de92725
Assadi <assadi.halifa@gmail.com>
2021-08-18 21:01:02 +0200

    update

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-const app = require("express")();
+const express = require("express");
+const path = require("path");
+const crypto = require("crypto");
+const twig = require("twig");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use("/assets", express.static(path.join(__dirname, "assets")));
+app.set("twig options", {
+  allow_async: true, // Allow asynchronous compiling
+  strict_variables: false,
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+/**
+ * app.use(express.json());
+ * app.use(express.urlencoded({ extended: true }));
+ *
+ */
+
+const generateKey = (param) => {
+  let value = parseInt(param);
+  return crypto.randomBytes(value).toString("hex");
+};
+
+app.get("/", (req, res) => {
+  res.render("home/index.html.twig");
+});
+
+app.post("/api/keyHex", (req, res) => {
+  const digit = req.body.digit;
+  const key = generateKey(digit);
+  res.status(200).json({ meassage: "ok", keygen: key });
+});
+
+app.listen(5000);
---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+const generatForm = document.querySelector("#onGenerateForm");
+const submitBtn = document.querySelector("#submitBtn");
+const digit = document.querySelector("#digit");
+const keyString = document.querySelector("#keyString");
+
+const host = window.location.href;
+submitBtn.setAttribute("disabled", true);
+
+const digitValueControl = () => {
+  let value = parseInt(digit.value);
+  if (value > 0) {
+    submitBtn.removeAttribute("disabled");
+  } else {
+    submitBtn.setAttribute("disabled", true);
+  }
+};
+
+const showKeygen = (arg) => {
+  keyString.textContent = arg;
+};
+
+const onGeneratorSubmit = (event) => {
+  event.preventDefault();
+  if (isNaN(event.target.digit.value)) {
+    return alert("Veuillez entrez une valeur numérique");
+  }
+  let data = { digit: event.target.digit.value };
+  fetch(host + "api/keyHex", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: { "Content-type": "application/json; charset=UTF-8" },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      showKeygen(data.keygen);
+    })
+    .catch((error) => {
+      alert("Une erreur est survenue: " + error);
+    });
+};
+
+digit.addEventListener("change", digitValueControl);
+digit.addEventListener("keyup", digitValueControl);
+generatForm.addEventListener("submit", onGeneratorSubmit);
